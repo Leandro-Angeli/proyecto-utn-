22444f702a4c48d728b48d272625814c083b692a
Leandro-Angeli <kurco91@hotmail.com>
2021-05-25 14:53:38 -0300

    beforo accesibility test

--- js/index.js
+++ js/index.js
@@ -2,11 +2,23 @@
 
   
   var h2 = document.getElementsByTagName('h2');
-
+  
+ 
+  
 dropdownBtn.addEventListener('click',()=>{
    navMenu.classList.toggle('nav-visible') ;
     header = document.querySelector('#header-move');
    header.classList.toggle('header-move');
+   var aria =  dropdownBtn.getAttribute('aria-expanded');
+   if (aria === true ) {
+     aria= false;
+     
+   } else { aria= true;
+     
+   }
+   dropdownBtn.setAttribute('aria-expanded',aria);
+  
+   
 
 
 
