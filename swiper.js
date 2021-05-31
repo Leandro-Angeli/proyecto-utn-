bfe3f6990ac4ca89498148ae3b6a7e6e0ca2914a
Leandro-Angeli <kurco91@hotmail.com>
2021-05-31 19:35:32 -0300

    slide setup

--- swiper.js
+++ swiper.js
@@ -1,6 +1,34 @@
 //SWIPER 
 var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 4,
+  slidesPerView: 6,
+  loop: true,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    hide: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+     
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+     
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+     
+    }
+  }
+  
+
  
 });
 
