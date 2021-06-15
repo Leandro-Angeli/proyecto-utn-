29f0913de044fa4d55499b1430ba8fe07e6b5f96
Leandro-Angeli <kurco91@hotmail.com>
2021-06-14 21:41:42 -0300

    eslint on

--- swiper.js
+++ swiper.js
@@ -1,38 +1,38 @@
 //SWIPER 
 var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 6,
-  loop: true,
-  scrollbar: {
-    el: '.swiper-scrollbar',
-    hide: false,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
+	slidesPerView: 6,
+	loop: true,
+	scrollbar: {
+		el: '.swiper-scrollbar',
+		hide: false,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	breakpoints: {
+		// when window width is >= 320px
+		320: {
+			slidesPerView: 1,
      
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 3,
+		},
+		// when window width is >= 480px
+		480: {
+			slidesPerView: 3,
      
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 4,
+		},
+		// when window width is >= 640px
+		640: {
+			slidesPerView: 4,
      
-    }
-  }
+		}
+	}
   
 
  
 });
 
-  //SWIPER 
+//SWIPER 
   
   
   
